ddf0bda791b273a83e83d953671fa91c18b971d6
Steve Annessa <steve.annessa@ziplist.com>
2012-12-10 21:27:28 -0500

    Messing around with python before the good stuff begins

---
+++ good_morning.py
@@ -0,0 +1,8 @@
+person = raw_input("Please enter your name: ")
+print "Good morning " + person
+weather = raw_input("Would you like to know the weather? ")
+if weather == "y":
+  print "Shitty, don't ride"
+elif weather == "n":
+  print "Best of luck with your guessing game"
+
